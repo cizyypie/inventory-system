1d793bf0fa4efb1412ed7822c0b45f5f393bb4f9
Joan <joanitatimbinp@gmail.com>
2026-04-14 22:32:50 +0700

    setup test fixture

---
+++ tests/fixtures/category.fixture.js
@@ -0,0 +1,6 @@
+export const categoryOne = {
+  name: 'laptop'
+}
+
+// store the created category id for later use
+export let categoryOneId = null
---
+++ tests/fixtures/user.fixture.js
@@ -0,0 +1,17 @@
+// regular user data
+export const userOne = {
+  name: 'user1',
+  email: 'user1@gmail.com',
+  password: 'user1111', 
+}
+
+// admin data  
+export const adminUser = {
+  name: 'admin1',
+  email: 'admin1@gmail.com',
+  password: 'admin1111',
+  role: 'admin'
+}
+
+export let userOneToken = null
+export let adminToken = null
