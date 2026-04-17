3b586d7f35e773dc8c699f6d88db418c333834b1
Joan <joanitatimbinp@gmail.com>
2026-04-17 22:58:52 +0700

    refactor: simplify app.js by removing redundant comments and consolidating middleware usage

--- src/app.js
+++ src/app.js
@@ -19,46 +19,23 @@ if (config.env !== 'test') {
   app.use(morganErrorHandler);
 }
 
-// set security HTTP headers
 app.use(helmet());
-
-// aktifin parsing json
 app.use(express.json());
-
-// aktifin urlencoded
 app.use(express.urlencoded({ extended: true }));
-
-// sanitize request data
 app.use(xss());
-
-// gzip compression
 app.use(compression());
-
-// enable cors
 app.use(cors());
 app.options('*', cors());
-
-// jwt authentication
 app.use(passport.initialize());
 passport.use('jwt', jwtStrategy);
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
-// api routes
+app.get('/', (req, res) => {res.send('hello world')});
 app.use('/', router);
-
-// app.use(router);
-
-// send 404 error jika route tidak ada
 app.use((req, res, next) => {
   next(new ApiError(status.NOT_FOUND, 'Not found'));
 });
 
-// convert error jadi Instance API Error jika ada error yang tidak ketangkap
 app.use(errorConverter);
-
-// handle error
 app.use(errorHandler);
 
 export default app;
